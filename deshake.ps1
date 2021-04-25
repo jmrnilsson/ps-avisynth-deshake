e01ff9e244a744f620927603bfce34f8ee01f31c
jmrnilsson <martin.jr.nilsson@gmail.com>
2021-04-25 10:12:35 +0200

    change paths for now. also removed aliases per vscode ps recommendations

--- deshake.ps1
+++ deshake.ps1
@@ -12,17 +12,17 @@ If (!$(Test-Path $path))
     Write-Error "'{0}' doesn't exist" -f $path
     exit 10
 }
-ElseIf ($(gci $path).PSIsContainer)
+ElseIf ($(Get-ChildItem $path).PSIsContainer)
 {
     Write-Error "Merge not supported at this time"
     exit 20
-    $files = gci $path | ?{ !$_.PSIsContainer } | foreach { $_.FullName }
+    $files = Get-ChildItem $path | ?{ !$_.PSIsContainer } | ForEach-Object { $_.FullName }
 }
 Else
 {
-    $name = $(gci $path).BaseName # | $_.Name
-    $outDirectory = $(gci $path).Directory.FullName
-    $output = gci $path | foreach { $_.BaseName + '.' +  ($(get-date -format s) -replace ':|-', '') + '.AVI' }
+    $name = $(Get-ChildItem $path).BaseName # | $_.Name
+    $outDirectory = $(Get-ChildItem $path).Directory.FullName
+    $output = Get-ChildItem $path | ForEach-Object { $_.BaseName + '.' +  ($(get-date -format s) -replace ':|-', '') + '.AVI' }
     $outFullName = $($outDirectory + '\' + $output)
 
     Write-Output $('path={2}, name={0}, output={1}' -f $name, $output, $path)
@@ -49,8 +49,8 @@ $script2 = $('deshake.{0}.pass2.avs' -f $name)
 
 $frameCount = ((Get-Content $log) -match '\d+')[1]
 
-$scriptFullName1 = (gci $script1).FullName -replace '\\', '\\'
-$scriptFullName2 = (gci $script2).FullName -replace '\\', '\\'
+$scriptFullName1 = (Get-ChildItem $script1).FullName -replace '\\', '\\'
+$scriptFullName2 = (Get-ChildItem $script2).FullName -replace '\\', '\\'
 $out = $outFullName -replace '\\', '\\'
 
 ((Get-Content 'deshake.template.jobs') | Out-String) -f $scriptFullName1, $scriptFullName2, $out, $frameCount | Set-Content $('deshake.{0}.all.jobs' -f $name)
